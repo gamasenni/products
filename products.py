d3fc0dced16a1f936ec5d517de23add92f30d882
Hsin <ydnicnai@gmail.com>
2020-05-12 13:53:54 +0800

    vers.5

--- products.py
+++ products.py
@@ -1,4 +1,14 @@
+#讀取檔案
 products = []
+with open ('product.csv', 'r', encoding = 'utf-8') as f :
+	for line in f :
+		if '商品,價格' in line :
+			continue#跳到下一迴
+		name, price = line.strip().split(',')
+		products.append([name,price])
+print(products)
+
+
 while True :
 	name = input('請輸入商品名稱: ')
 	if name == 'q':
@@ -17,3 +27,4 @@ with open('product.csv', 'w', encoding = 'utf-8') as f :
 		f.write(p[0] + ',' + str(p[1]) + '\n')
 		#有逗點和換行符號區隔，excel才會分隔
 		#str(p[1])轉換為字串才能用加法合併
+
