bdf3e9bf979f42d3cc6f2434abf3ebf4cd8ddf13
Hsin <ydnicnai@gmail.com>
2020-05-12 13:27:45 +0800

    vers.4

--- products.py
+++ products.py
@@ -4,13 +4,16 @@ while True :
 	if name == 'q':
 		break
 	price = input('請輸入商品價格: ')
+	price = int(price)
 	products.append([name,price])
 print(products)
 
 for p in products :
 	print(p[0], '的價格是', p[1])
 
-with open('product.csv', 'w') as f :
+with open('product.csv', 'w', encoding = 'utf-8') as f :
+	f.write('商品,價格\n')
 	for p in products :
-		f.write(p[0] + ',' + p[1] + '\n')
+		f.write(p[0] + ',' + str(p[1]) + '\n')
 		#有逗點和換行符號區隔，excel才會分隔
+		#str(p[1])轉換為字串才能用加法合併
